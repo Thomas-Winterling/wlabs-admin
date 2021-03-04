19acdda9276f1b811f3bb5db0be58811f9d5b9c1
Thomas Winterling <winterlinglabs@gmail.com>
2021-03-04 07:13:20 +0100

    Create content

--- src/components/pages/Calendar.js
+++ src/components/pages/Calendar.js
@@ -1,11 +1,93 @@
-import React from 'react'
+import React, { useState, useEffect } from "react"
+import Header from '../Header'
+import Sidebar from '../Sidebar'
+import BeatLoader from "react-spinners/BeatLoader"
+import { css } from "@emotion/core";
 
-function Calendar() {
-    return (
-        <div>
-            Calendar
-        </div>
-    )
+
+
+export default function Calendar() {
+  //Loading Spinner
+  const [loading, setLoading] = useState(false);
+  const [content, setContent] = useState('none');
+  
+ // Style Spinner and set Spinner
+ const override = css`
+ display: block;
+ position: absolute;
+ top: 50%;
+ left: 50%;
+ margin: 0 auto;
+ border-color: red;
+`;
+
+ useEffect(()=>{
+   setLoading(true)
+   setTimeout(() => {
+     setLoading(false)
+     setContent('flex')
+   }, 3000)
+ },[])
+
+  return (
+    <>
+      <div className="calendar">
+      <div className="top-container">
+        <Header />
+      </div>
+      <div className="content-container">
+      <div className="content-left">
+        <Sidebar />
+      </div>
+        <div className="content-right">
+          {
+            loading ?
+
+            <BeatLoader
+              color="orange"
+              loading={loading}
+              css={override}
+              size={30} 
+            />
+
+            :
+
+            <div style={{display: content, flexDirection: "column" }}>
+              <div className="headline">
+                <h4>Calendar</h4>
+              </div>
+              <div className="todos">
+                <div className="todos-header">
+                  
+                </div>
+
+               
+
+            <div className="todos-content">
+           
+              
+              
+                </div>
+              </div>
+
+              <div className="statistics">
+                <div className="wl-statistic">
+                  
+                </div>
+                <div className="wl-statistic">
+                  
+                </div>
+                <div className="wl-statistic">
+                  
+                </div>
+              </div>
+              
+              
+            </div>
+          }
+      </div>
+    </div>
+  </div>  
+  </>
+  )
 }
-
-export default Calendar
--- src/components/pages/Emails.js
+++ src/components/pages/Emails.js
@@ -1,11 +1,93 @@
-import React from 'react'
+import React, { useState, useEffect } from "react"
+import Header from '../Header'
+import Sidebar from '../Sidebar'
+import BeatLoader from "react-spinners/BeatLoader"
+import { css } from "@emotion/core";
 
-function Emails() {
-    return (
-        <div>
-            Emails
-        </div>
-    )
+
+
+export default function Emails() {
+  //Loading Spinner
+  const [loading, setLoading] = useState(false);
+  const [content, setContent] = useState('none');
+  
+ // Style Spinner and set Spinner
+ const override = css`
+ display: block;
+ position: absolute;
+ top: 50%;
+ left: 50%;
+ margin: 0 auto;
+ border-color: red;
+`;
+
+ useEffect(()=>{
+   setLoading(true)
+   setTimeout(() => {
+     setLoading(false)
+     setContent('flex')
+   }, 3000)
+ },[])
+
+  return (
+    <>
+      <div className="emails">
+      <div className="top-container">
+        <Header />
+      </div>
+      <div className="content-container">
+      <div className="content-left">
+        <Sidebar />
+      </div>
+        <div className="content-right">
+          {
+            loading ?
+
+            <BeatLoader
+              color="orange"
+              loading={loading}
+              css={override}
+              size={30} 
+            />
+
+            :
+
+            <div style={{display: content, flexDirection: "column" }}>
+              <div className="headline">
+                <h4>Emails</h4>
+              </div>
+              <div className="todos">
+                <div className="todos-header">
+                  
+                </div>
+
+               
+
+            <div className="todos-content">
+           
+              
+              
+                </div>
+              </div>
+
+              <div className="statistics">
+                <div className="wl-statistic">
+                  
+                </div>
+                <div className="wl-statistic">
+                  
+                </div>
+                <div className="wl-statistic">
+                  
+                </div>
+              </div>
+              
+              
+            </div>
+          }
+      </div>
+    </div>
+  </div>  
+  </>
+  )
 }
-
-export default Emails
